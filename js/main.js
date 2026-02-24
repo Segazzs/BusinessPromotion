c7e6a17827007bacd7b5b98f0d01cdbae5c10014
Segazzzs <mrakul986@gmail.com>
2026-02-24 16:08:51 +0200

    add script and modal block for mobile menu

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-console.log("Hello");
+const menuOpenBtn = document.getElementById("menu-open-btn");
+const modalContent = document.getElementById("modal-content");
+
+menuOpenBtn.addEventListener("click", function () {
+  modalContent.classList.toggle("modal-open");
+});
+
+console.log(menuOpenBtn);
